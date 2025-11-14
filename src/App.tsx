12b8d3ca1cfde1fd90b0fadd534d093952b1925e
arun909 <arunbabu7591@gmail.com>
2025-11-14 14:23:19 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/Login/LoginPage'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<LoginPage/>}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
